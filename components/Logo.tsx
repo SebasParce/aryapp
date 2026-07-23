30a7196665bc85e8f2e5ec21b896754e374fb053
Sebastian Arce <sebastianarcerubio@gmail.com>
2026-07-23 14:17:11 -0400

    Usar img nativo en vez de next/image para el logo (evitar el pipeline de optimizacion de Vercel)

--- components/Logo.tsx
+++ components/Logo.tsx
@@ -1,17 +1,13 @@
-import Image from "next/image";
-
 export default function Logo({
   className = "h-8",
 }: {
   className?: string;
 }) {
+  // eslint-disable-next-line @next/next/no-img-element
   return (
-    <Image
+    <img
       src="/logo.png"
       alt="BucketsAi"
-      width={375}
-      height={90}
-      priority
       className={`${className} w-auto object-contain`}
     />
   );
